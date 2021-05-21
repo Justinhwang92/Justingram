69cfcbf230dd50f0a44975482ddccc3b0fa998db
Justin <justinhwang92@gmail.com>
2021-05-21 14:59:00 -0400

    Designing the UI from Instagram

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5"><img src="https://lh3.googleusercontent.com/Krj7U36DaLwvUl1lRaZsNBhIw06Ftayl7OakgXOcSPJu9P2xYSLenKflvZdwmDy1gXiXo6zvfIk1wG8l9vjqRcnm_Q0ZBJnaetyOtZ5fuVhO_rGR9OeX-oGbHyRAyCE47T5iWzyeUDCdhORXHTmZ09EN4r-GphkUkIekOfbIpfiYx-JVI4ofzochgzr0U9uba0SENp9RTxdnV1X5KIYDGngg5eQGVVyOlbV52dlyYbpRzPrhj34y9Hx7t77VzajxlJCI4fefb08Srkx9QmFUUhM-aAE91kPm_6yPS6yCIVHFWEAE2QXlL996wY6j1g-PRRO97EkvbaTc5Cmwf4pQe104tFRjL25vYajPyS4gJz9debht0SDzVS-dkEOIbBYhfQkYFABoCJm_swgeS4IpH1ce5hnXBLICj8Beoq-k_AZF6CGBNnPngvwRb_HF-RXURpoqtdcWYGXKrK73cJh-q8DBFhUjla_t3kOsw3PQHXt5ueUUHx5StLJLicoLxWZqV1XA51Lmqd_oPDOWdVfE74wNrPK0GR17mVTjsl66ugdSbf0e_caHx0yEkftErWQg1D4ufDdxcUys_xO6X6lKpN2gv2d-g4JNADqnDGzHz_k2cbKYoRBqqZavJL4JHlM-R22d11x8d6IMJEBd-q0rptIC5WUB_bAq-d3H0V50ioRdeeHvDaZuvYTJk0CphjOkZ8zudp7SLQtuaUKW3ZZKVCBMpQ=w328-h262-no?authuser=0" alt="profile" class="rounded-circle" style="height:150px;"></div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>JustinCoding</h1>
             </div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>2568</strong> Posts</div>
+                <div class="pr-4"><strong>19.7M</strong> Followers</div>
+                <div class="pr-4"><strong>0</strong> Folloing</div>
+            </div>
+            <div class="pt-4 font-weight-bold">Justin Hwang</div>
+            <div>Let's code your dream with Justin! Web / App / Frontend / Backend / Full-stack development</div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://cdn-media-1.freecodecamp.org/images/1*rV6_99F42Jr7uR5YtLrHrQ.jpeg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://miro.medium.com/max/5100/1*CFM9_VhPRrGG755enCmClw.jpeg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://www.theweb.com.pk/user/img/new/web-development-in-karachi.jpg" class="w-100">
         </div>
     </div>
 </div>
