83b30b5a68e1015b23b9eafa1abbd770de32240e
Justin <justinhwang92@gmail.com>
2021-05-23 14:33:42 -0400

    Add RESTful resouce controller and passing data to the View

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-3 p-5"><img src="https://lh3.googleusercontent.com/Krj7U36DaLwvUl1lRaZsNBhIw06Ftayl7OakgXOcSPJu9P2xYSLenKflvZdwmDy1gXiXo6zvfIk1wG8l9vjqRcnm_Q0ZBJnaetyOtZ5fuVhO_rGR9OeX-oGbHyRAyCE47T5iWzyeUDCdhORXHTmZ09EN4r-GphkUkIekOfbIpfiYx-JVI4ofzochgzr0U9uba0SENp9RTxdnV1X5KIYDGngg5eQGVVyOlbV52dlyYbpRzPrhj34y9Hx7t77VzajxlJCI4fefb08Srkx9QmFUUhM-aAE91kPm_6yPS6yCIVHFWEAE2QXlL996wY6j1g-PRRO97EkvbaTc5Cmwf4pQe104tFRjL25vYajPyS4gJz9debht0SDzVS-dkEOIbBYhfQkYFABoCJm_swgeS4IpH1ce5hnXBLICj8Beoq-k_AZF6CGBNnPngvwRb_HF-RXURpoqtdcWYGXKrK73cJh-q8DBFhUjla_t3kOsw3PQHXt5ueUUHx5StLJLicoLxWZqV1XA51Lmqd_oPDOWdVfE74wNrPK0GR17mVTjsl66ugdSbf0e_caHx0yEkftErWQg1D4ufDdxcUys_xO6X6lKpN2gv2d-g4JNADqnDGzHz_k2cbKYoRBqqZavJL4JHlM-R22d11x8d6IMJEBd-q0rptIC5WUB_bAq-d3H0V50ioRdeeHvDaZuvYTJk0CphjOkZ8zudp7SLQtuaUKW3ZZKVCBMpQ=w328-h262-no?authuser=0" alt="profile" class="rounded-circle" style="height:150px;"></div>
         <div class="col-9 pt-5">
             <div>
-                <h1>JustinCoding</h1>
+                <h1>{{ $user->username }}</h1>
             </div>
             <div class="d-flex">
                 <div class="pr-4"><strong>2568</strong> Posts</div>
